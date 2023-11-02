4a3cc86125f969197d635ad91f76b6dcfdb8c75a
HDUY <leduy020702@gmail.com>
2023-11-02 22:15:40 +0700

    simulation atm with java project

--- src/CayATM_GUI/TransactionHistory.java
+++ src/CayATM_GUI/TransactionHistory.java
@@ -102,34 +102,61 @@ public class TransactionHistory extends javax.swing.JPanel implements IKeyCodeOb
 
     private void myInits() {
         String[] columns = {"Mã", "Ngày", "Tiền", "ND"};
-        DefaultTableModel tblModel=new DefaultTableModel(columns,0);
-//        tblModel =getDataTransactionHistory(tblModel);
+//        DefaultTableModel tblModel=new DefaultTableModel(columns,0);
+        DefaultTableModel tblModel=new DefaultTableModel();
+        tblModel.setColumnIdentifiers(columns);
+        tblHistory.setModel(tblModel);
+        getDataTransactionHistory();
+//        DefaultTableModel tblModel1=new DefaultTableModel(Model,columns);
 //        TableModel tbl=tblHistory.getModel();
 //        DefaultTableModel dtmHistory = (DefaultTableModel) tbl;
 //        tblHistory.setModel(dtmHistory);
 //        JScrollPane sp= new JScrollPane(tblHistory);
 //        sp.setBounds(0, 0, 490, 250);
 //        this.add(sp);
+        tblModel.addTableModelListener(tblHistory);
     }
-
+    
     private void getDataTransactionHistory() {
-        String data[][]=null;
+        String data[]=new String[4];
+        DefaultTableModel tbl = new DefaultTableModel();
         long idSTK = CayATM.currentCard.getIdSTK();
         List<TransactionInfo> transactionInfos = CayATM.trans.xemLichSu(idSTK);
         // Tiến hành đổ dữ liệu vào mảng data
         if (transactionInfos != null) {
             for (int i = 0; i < transactionInfos.size(); i++) {
                 TransactionInfo transInfo = transactionInfos.get(i);
-                data[i][0] = ""+transInfo.getIdTransaction();
-                data[i][1] = ""+transInfo.getNgayGD().toString();
-                data[i][2] = ""+transInfo.getSoTienGD();
-                data[i][3] = ""+transInfo.getNoiDung();
+                data[0] = ""+transInfo.getIdTransaction();
+                data[1] = ""+transInfo.getNgayGD().toString();
+                data[2] = ""+transInfo.getSoTienGD();
+                data[3] = ""+transInfo.getNoiDung();
                 
-//                model.addRow(tb);
+                tbl.addRow(data);
             }
-//            tblHistory.setModel(model);
+            tblHistory.setModel(tbl);
         }
     }
+
+//    private String[][] getDataTransactionHistory() {
+//        String data[][]=null;
+//        long idSTK = CayATM.currentCard.getIdSTK();
+//        List<TransactionInfo> transactionInfos = CayATM.trans.xemLichSu(idSTK);
+//        // Tiến hành đổ dữ liệu vào mảng data
+//        if (transactionInfos != null) {
+//            TableModel tbl=tblHistory.getModel();
+//            for (int i = 0; i < transactionInfos.size(); i++) {
+//                TransactionInfo transInfo = transactionInfos.get(i);
+//                data[i][0] = ""+transInfo.getIdTransaction();
+//                data[i][1] = ""+transInfo.getNgayGD().toString();
+//                data[i][2] = ""+transInfo.getSoTienGD();
+//                data[i][3] = ""+transInfo.getNoiDung();
+//                
+//                tbl.addTableModelListener(data);
+//            }
+////            tblHistory.setModel(model);
+//        }
+//        return data;
+//    }
 
     @Override
     public void updateKeyCodeStatus(KeyboardEnum keyCode) {
